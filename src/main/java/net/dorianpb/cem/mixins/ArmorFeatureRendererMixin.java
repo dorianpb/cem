2df94bad63ad858ec3152aed67e0d79ef0181072
dorianpb <dorian.peanut.butter@gmail.com>
2021-10-02 20:29:58 -0400

    make it so the previous commit doesn't forcibly remove textures from armor models

--- src/main/java/net/dorianpb/cem/mixins/ArmorFeatureRendererMixin.java
+++ src/main/java/net/dorianpb/cem/mixins/ArmorFeatureRendererMixin.java
@@ -4,13 +4,9 @@ import net.dorianpb.cem.internal.api.CemModel;
 import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ArmorFeatureRenderer.class)
 public class ArmorFeatureRendererMixin<T extends LivingEntity>{
@@ -24,10 +20,4 @@ public class ArmorFeatureRendererMixin<T extends LivingEntity>{
 			child.rightArm.pitch += 1.57;
 		}
 	}
-	
-	@Inject(method = "getArmorTexture", at = @At(value = "HEAD"), cancellable = true)
-	private void yeah(ArmorItem item, boolean legs, String overlay, CallbackInfoReturnable<Identifier> cir){
-		cir.setReturnValue(new Identifier("not_here.png"));
-	}
-	
 }
