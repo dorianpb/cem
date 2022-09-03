8d327f41e2c4e3ca1ac09e97aaa38f98d63f976d
dorianpb <dorian.peanut.butter@gmail.com>
2022-09-03 15:03:46 -0400

    remove cem entrypoint system

--- src/main/java/net/dorianpb/cem/internal/CemInit.java
+++ src/main/java/net/dorianpb/cem/internal/CemInit.java
@@ -1,48 +1,13 @@
 package net.dorianpb.cem.internal;
 
-import net.dorianpb.cem.internal.api.CemEntityInitializer;
 import net.dorianpb.cem.internal.config.CemConfigFairy;
-import net.dorianpb.cem.internal.util.CemFairy;
-import net.dorianpb.cem.mixins.BlockEntityRendererAccessor;
-import net.dorianpb.cem.mixins.EntityRendererAccessor;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.metadata.ModMetadata;
 
 
 public class CemInit implements ClientModInitializer{
 	@Override
-	@SuppressWarnings({"unchecked", "RedundantSuppression"})
 	public void onInitializeClient(){
 		CemConfigFairy.loadConfig();
-		FabricLoader.getInstance().getEntrypointContainers("cem", CemEntityInitializer.class).forEach((container) -> {
-			ModMetadata provider = container.getProvider().getMetadata();
-			CemEntityInitializer entrypoint = container.getEntrypoint();
-			
-			entrypoint.onInit();
-			CemFairy.getLogger().info("Loading " + entrypoint.getSize() + " entities from " + provider.getName() + " " + provider.getVersion());
-			entrypoint.getCemEntityFactories().forEach((type, factory) -> {
-				if(CemFairy.addSupport(type)){
-					EntityRendererAccessor.callRegister(type, factory);
-				}
-				else{
-					CemFairy.getLogger().error("Entity type " + type + " already present!");
-				}
-			});
-			entrypoint.getCemBlockEntityFactories().forEach((type, factory) -> {
-				if(CemFairy.addSupport(type)){
-					BlockEntityRendererAccessor.callRegister(type, factory);
-				}
-				else{
-					CemFairy.getLogger().error("Entity type " + type + " already present!");
-				}
-			});
-			entrypoint.getCemOthers().forEach((type) -> {
-				if(!CemFairy.addSupport(type)){
-					CemFairy.getLogger().error("Entity type " + type + " already present!");
-				}
-			});
-		});
 	}
 }
 
